32f80e59e11864a9bc26ec0226711085745538a4
SimonZarni <simonallen364@gmail.com>
2026-03-19 18:22:16 +0700

    Fix issues

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,7 @@ use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\Admin\AdminReviewController;
 use App\Http\Controllers\Admin\AdminCustomerController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -108,7 +92,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
         // Reviews
         Route::get('/reviews',                [AdminReviewController::class, 'index'])->name('reviews.index');
-        Route::patch('/reviews/{id}/approve', [AdminReviewController::class, 'approve'])->name('reviews.approve');
         Route::delete('/reviews/{id}',        [AdminReviewController::class, 'reject'])->name('reviews.reject');
 
         // Customers
